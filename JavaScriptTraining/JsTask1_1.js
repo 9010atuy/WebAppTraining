03d78ae8edb9bd4829da012b3abf24e2fcfb2d96
yuta <9010atuy@gmail.copm>
2021-07-03 01:29:12 +0900

    FizzBuzzをJavaScriptで作成

---
+++ JavaScriptTraining/JsTask1_1.js
@@ -0,0 +1,10 @@
+const MAX_COUNT = 99;
+for (var i = 1; i <= MAX_COUNT; i++){
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(`FizzBuzz ${i}`);
+    } else if (i % 3 == 0) {
+        console.log(`Fizz ${i}`);
+    } else if (i % 5 == 0) {
+        console.log(`Buzz ${i}`);
+    }
+}
