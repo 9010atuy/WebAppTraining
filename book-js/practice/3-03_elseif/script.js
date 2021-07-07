fca8855edbd1322df902223d37043ca9e543dd46
yuta <9010atuy@gmail.copm>
2021-07-08 01:39:58 +0900

    add else if

---
+++ book-js/practice/3-03_elseif/script.js
@@ -0,0 +1,8 @@
+var answer = window.prompt('Help?');
+if (answer === 'yes') {
+  alert('Tap to jump, Avoid obstacles');
+} else if (answer === 'no') {
+  window.alert('loading...');
+} else {
+  window.alert('Answer "yes" or "no"');
+}
