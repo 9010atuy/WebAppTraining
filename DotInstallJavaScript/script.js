f1ea22e4e289b57db38258558f9490fdbcef4a01
yuta <9010atuy@gmail.copm>
2021-07-22 21:17:35 +0900

    add array

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,19 +1,9 @@
-"use strict";
-const element_count = 9;
-const correct_number = Math.floor(Math.random() * element_count + 1);
-for (let i = 0; i < element_count; i++) {
-  let div = document.createElement("div");
-  div.classList.add("btn");
-  if (i == correct_number) {
-    div.addEventListener("click", () => {
-      div.classList.add("circle");
-      div.innerHTML = "Win!";
-    });
-  } else {
-    div.addEventListener("click", () => {
-      div.classList.add("incorrect_animation");
-      div.innerHTML = "Lose";
-    });
-  }
-  document.body.appendChild(div);
-}
+'use strict';
+const scoreArray = [80, 90, 70];
+
+console.log(scoreArray);
+console.log(scoreArray.length);
+
+scoreArray[2] = 44;
+console.log(scoreArray);
+console.log(scoreArray.length);
