a8f92f97aa8c9eabc971be7a1418f1674ec30802
yuta <9010atuy@gmail.copm>
2021-07-27 01:12:29 +0900

    add push, shift, unshift, pop, splice

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -4,6 +4,13 @@ const scoreArray = [80, 90, 70];
 console.log(scoreArray);
 console.log(scoreArray.length);
 
-scoreArray[2] = 44;
-console.log(scoreArray);
-console.log(scoreArray.length);
+scoreArray.push(40, 50);
+scoreArray.shift();
+scoreArray.unshift(20);
+scoreArray.pop();
+
+scoreArray.splice(0, 1);
+
+scoreArray.forEach((score, index) => {
+  console.log(`${index}, ${score}`);
+});
