13926b1efaaf752edfeb810fad9efc0068278757
yuta <9010atuy@gmail.copm>
2021-08-05 22:49:48 +0900

    array: spread syntax

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,16 +1,14 @@
 'use strict';
 const scoreArray = [80, 90, 70];
+const otherScoreArray = [10, 20, 30];
 
+scoreArray.push(otherScoreArray);
 console.log(scoreArray);
-console.log(scoreArray.length);
+scoreArray.push(...otherScoreArray);
+console.log(scoreArray);
 
-scoreArray.push(40, 50);
-scoreArray.shift();
-scoreArray.unshift(20);
-scoreArray.pop();
+const sum = (a, b) => {
+  return a + b;
+};
 
-scoreArray.splice(0, 1);
-
-scoreArray.forEach((score, index) => {
-  console.log(`${index}, ${score}`);
-});
+console.log(sum(...scoreArray));
