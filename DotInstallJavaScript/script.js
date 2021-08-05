58a606fb7d1fcd1addf63affae4fd5cd8379fdbd
yuta <9010atuy@gmail.copm>
2021-08-05 23:04:50 +0900

    add split assignment

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,14 +1,21 @@
 'use strict';
-const scoreArray = [80, 90, 70];
-const otherScoreArray = [10, 20, 30];
 
-scoreArray.push(otherScoreArray);
-console.log(scoreArray);
-scoreArray.push(...otherScoreArray);
-console.log(scoreArray);
+// split assignment
 
-const sum = (a, b) => {
-  return a + b;
-};
+const appleLapTop = ['Macbook Pro 13', 'Macbook Pro 16', 'Macbook Air 13'];
 
-console.log(sum(...scoreArray));
+const [standardLaptop, hiendLaptop, rightLaptop] = appleLapTop;
+
+const [popularLaptop, ...otherLaptops] = appleLapTop;
+
+console.log(standardLaptop, hiendLaptop, rightLaptop);
+console.log(popularLaptop);
+console.log(otherLaptops);
+console.log(...otherLaptops);
+
+const [mini, pro] = ['$780', '$1500'];
+console.log(`mini: ${mini}`);
+console.log(`pro: ${pro}`);
+const [miniP, proM] = [pro, mini];
+console.log(`miniP: ${miniP}`);
+console.log(`proM: ${proM}`);
