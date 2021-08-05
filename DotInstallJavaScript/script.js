26486f12a88bb97608f39c0e4fd3d1a16f519520
yuta <9010atuy@gmail.copm>
2021-08-05 23:10:23 +0900

    add map

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -2,20 +2,7 @@
 
 // split assignment
 
-const appleLapTop = ['Macbook Pro 13', 'Macbook Pro 16', 'Macbook Air 13'];
+const appleLapTops = ['Macbook Pro 13', 'Macbook Pro 16', 'Macbook Air 13'];
 
-const [standardLaptop, hiendLaptop, rightLaptop] = appleLapTop;
-
-const [popularLaptop, ...otherLaptops] = appleLapTop;
-
-console.log(standardLaptop, hiendLaptop, rightLaptop);
-console.log(popularLaptop);
-console.log(otherLaptops);
-console.log(...otherLaptops);
-
-const [mini, pro] = ['$780', '$1500'];
-console.log(`mini: ${mini}`);
-console.log(`pro: ${pro}`);
-const [miniP, proM] = [pro, mini];
-console.log(`miniP: ${miniP}`);
-console.log(`proM: ${proM}`);
+const newAppleLapTops = appleLapTops.map(laptop => laptop + ' 2021 Oct');
+newAppleLapTops.forEach(newLaptop => console.log(newLaptop));
