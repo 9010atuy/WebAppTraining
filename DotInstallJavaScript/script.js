5c6134bb8574e6a04f8dd003a4078abac00047c3
yuta <9010atuy@gmail.copm>
2021-08-17 02:15:25 +0900

    str operate by join, split

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,6 +1,20 @@
 'use strict';
-const str = 'snow board';
 
-console.log(`length: ${str.length}`);
-console.log(`substring: ${str.substring()}`);
-console.log(`last char: ${str[str.length - 1]}`);
+const dateNow = new Date();
+const year = dateNow.getFullYear();
+const month = dateNow.getMonth();
+const day = dateNow.getDay();
+
+const dateArray = [year, month, day];
+console.log(dateArray);
+
+const strDate = dateArray.join('.');
+console.log(strDate);
+
+const dArray = strDate.split('.');
+console.log(dArray);
+
+const [y, m, d] = dateArray;
+console.log(y);
+console.log(m);
+console.log(d);
