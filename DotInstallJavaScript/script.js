362a815fe0350eb63f2570df2bca2e3ed3d31322
yuta <9010atuy@gmail.copm>
2021-08-17 01:59:10 +0900

    use string method

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,58 +1,6 @@
 'use strict';
+const str = 'snow board';
 
-// split assignment
-
-const appleLapTops = ['Macbook Pro 13', 'Macbook Pro 16', 'Macbook Air 13'];
-const series2021 = appleLapTops.map(laptop => laptop + ' 2021');
-console.log(...series2021);
-const proSeries = appleLapTops.filter(laptop => laptop.indexOf('Pro') !== -1);
-console.log(...proSeries);
-
-const laptopPro13 = {
-  name: 'Macbook Pro 13',
-  price: '$130',
-  stock: 20,
-};
-console.log(laptopPro13);
-console.log(laptopPro13.name);
-console.log(laptopPro13['name']);
-console.log(laptopPro13.stock);
-
-// update propery value
-laptopPro13.stock = 18;
-console.log(laptopPro13.stock);
-
-// add property
-laptopPro13.color = 'dark gray';
-console.log(laptopPro13);
-
-// del property
-delete laptopPro13.color;
-console.log(laptopPro13);
-
-// spread => object
-const otherProps = {
-  color: 'dark gray',
-  keybord: 'US',
-};
-
-const laptopPro16 = {
-  name: 'Macbook Pro 13',
-  price: '$130',
-  stock: 20,
-  ...otherProps,
-};
-
-console.log(laptopPro16);
-
-const { name, price, stock, color, keybord } = laptopPro16;
-console.log(name);
-console.log(price);
-console.log(stock);
-console.log(color);
-console.log(keybord);
-
-const pro16Keys = Object.keys(laptopPro16);
-pro16Keys.forEach(key => {
-  console.log(key, laptopPro16[key]);
-});
+console.log(`length: ${str.length}`);
+console.log(`substring: ${str.substring()}`);
+console.log(`last char: ${str[str.length - 1]}`);
