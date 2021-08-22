61344d6f354f354157f53b642901c529fec02244
yuta <9010atuy@gmail.copm>
2021-08-23 01:53:24 +0900

    add dice

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,16 +1,7 @@
 'use strict';
 
-const scores = [20, 30, 15];
-let sum = 0;
-scores.forEach(score => {
-  sum += score;
-});
-console.log(`sum: ${sum}`);
-const avg = sum / scores.length;
-console.log(`avg: ${avg}`);
-console.log(Math.floor(avg));
-console.log(Math.ceil(avg));
-console.log(Math.round(avg));
+const max = 6;
+const min = 1;
+const dice = Math.floor(Math.random() * (max + 1 - min) + min);
 
-const ramdom = Math.floor(Math.random(6) * 100);
-console.log(ramdom);
+console.log(dice);
