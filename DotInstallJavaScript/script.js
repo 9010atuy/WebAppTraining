29abdb277cb0b10a99472f7ed3988748c181eee5
yuta <9010atuy@gmail.copm>
2021-08-25 01:49:22 +0900

    add try-catch

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,25 +1,13 @@
 'use strict';
 
-let i = 0;
-const showTime = () => {
-  console.log(new Date());
-  i++;
-  const timeoutId = setTimeout(showTime, 1000);
-  if (i > 5) {
-    clearTimeout(timeoutId);
-  }
-};
+const strInput = 1;
+// const strInput = prompt('input string!');
 
-showTime();
+try {
+  const upperStr = strInput.toUpperCase();
+  console.log(upperStr);
+} catch (ex) {
+  console.log(ex);
+}
 
-let j = 0;
-const dice = () => {
-  console.log(Math.floor(Math.random() * 6 + 1));
-  j++;
-  console.log(`j: ${j}`);
-  if (j > 4) {
-    clearInterval(intervalId);
-  }
-};
-
-const intervalId = setInterval(dice, 1000);
+console.log('done');
