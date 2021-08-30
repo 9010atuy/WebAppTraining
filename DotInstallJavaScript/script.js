946f27d5880e749510475722dd19d6e281f9cc0b
yuta <9010atuy@gmail.copm>
2021-08-31 01:48:20 +0900

    add event listner

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const doSomething = () => {
-  // document.getElementById('title').textContent = 'changed';
-  // document.querySelector('p').textContent = 'changed';
   const pList = document.querySelectorAll('p');
   pList.forEach((elem, index) => {
-    console.log(elem);
     elem.textContent = `${index + 1}    changed`;
   });
 };
 
-setTimeout(doSomething, 1000);
+const submitButton = document.getElementById('submit');
+
+submitButton.addEventListener('click', doSomething);
