78e7c6531bebe74545e66ba3506f39fce3f51d9b
yuta <9010atuy@gmail.copm>
2021-08-31 01:43:28 +0900

    start dom

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,46 +1,13 @@
 'use strict';
 
-class Post {
-  constructor(text) {
-    this.text = text;
-    this.likeCount = 0;
-  }
+const doSomething = () => {
+  // document.getElementById('title').textContent = 'changed';
+  // document.querySelector('p').textContent = 'changed';
+  const pList = document.querySelectorAll('p');
+  pList.forEach((elem, index) => {
+    console.log(elem);
+    elem.textContent = `${index + 1}    changed`;
+  });
+};
 
-  show() {
-    console.log(`${this.text}    ${this.likeCount} like!!`);
-  }
-
-  like() {
-    this.likeCount++;
-    console.log('you get like!!');
-    this.show();
-  }
-
-  static classInfo() {
-    console.log('show class version');
-  }
-}
-
-class SponsoredPost extends Post {
-  constructor(text, sponsored) {
-    super(text);
-    this.sponsored = sponsored;
-  }
-  show() {
-    console.log(
-      `${this.text}    ${this.likeCount} like!!    spoonsered by ${this.sponsored}`
-    );
-  }
-}
-
-const posts = [
-  new Post('nice to meet you!'),
-  new Post('my name is yuta'),
-  new Post("I'm 29 old"),
-  new SponsoredPost('favarite GoldenKamui', 'Young Jamp'),
-];
-
-posts.forEach(post => {
-  post.show();
-  post.like();
-});
+setTimeout(doSomething, 1000);
