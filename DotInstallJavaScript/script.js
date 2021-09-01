6cf57f689372ac5cb2d169d0283d3cd50744e7fe
yuta <9010atuy@gmail.copm>
2021-09-01 22:03:14 +0900

    add change elem property

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -2,8 +2,11 @@
 
 const doSomething = () => {
   const pList = document.querySelectorAll('p');
-  pList.forEach((elem, index) => {
-    elem.textContent = `${index + 1}    changed`;
+  pList.forEach((node, index) => {
+    node.textContent = `${index + 1}    changed`;
+    node.title = `changed tooltip${index + 1}`;
+    node.style.backgroundColor = 'black';
+    node.style.color = 'white';
   });
 };
 
