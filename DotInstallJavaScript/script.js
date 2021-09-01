83063c6014aa3287cc6bd863cfe6e6e91e2e6d90
yuta <9010atuy@gmail.copm>
2021-09-01 22:25:24 +0900

    add classList

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -3,10 +3,12 @@
 const doSomething = () => {
   const pList = document.querySelectorAll('p');
   pList.forEach((node, index) => {
-    node.textContent = `${index + 1}    changed`;
-    node.title = `changed tooltip${index + 1}`;
-    node.style.backgroundColor = 'black';
-    node.style.color = 'white';
+    if (node.classList.contains('my-text-style')) {
+      node.className = '';
+    } else {
+      node.classList.add('my-text-style');
+      node.classList.add('my-border-style');
+    }
   });
 };
 
