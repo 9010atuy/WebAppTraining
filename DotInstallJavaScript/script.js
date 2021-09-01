595753a473bfd12d24bb819def8e43868d613ee6
yuta <9010atuy@gmail.copm>
2021-09-01 22:45:27 +0900

    add cloneNode, insertBefore

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,12 +1,23 @@
 'use strict';
-const button = document.getElementById('submit');
+const add = document.getElementById('add');
+const copy = document.getElementById('copy');
+const ul = document.querySelector('ul');
+
 let counter = 1;
-const clickEvent = () => {
-  const ul = document.querySelector('ul');
+const addF = () => {
   const li = document.createElement('li');
   li.textContent = `list${counter}`;
+  li.id = `list${counter}`;
   ul.appendChild(li);
   counter++;
 };
 
-button.addEventListener('click', clickEvent);
+const copyF = () => {
+  const list3 = document.querySelectorAll('li')[2];
+  const copyElem = list3.cloneNode('true');
+  const list5 = document.querySelectorAll('li')[4];
+  ul.insertBefore(copyElem, list5);
+};
+
+add.addEventListener('click', addF);
+copy.addEventListener('click', copyF);
