b0405be39dba609e1ab88e3bb748fc409739b689
yuta <9010atuy@gmail.copm>
2021-09-01 22:30:03 +0900

    add custom data attribute

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,17 +1,5 @@
 'use strict';
 
-const doSomething = () => {
-  const pList = document.querySelectorAll('p');
-  pList.forEach((node, index) => {
-    if (node.classList.contains('my-text-style')) {
-      node.className = '';
-    } else {
-      node.classList.add('my-text-style');
-      node.classList.add('my-border-style');
-    }
-  });
-};
-
-const submitButton = document.getElementById('submit');
-
-submitButton.addEventListener('click', doSomething);
+const titleNode = document.getElementById('title');
+// element.dataset.<after [data-] word>
+titleNode.title = titleNode.dataset.translate;
