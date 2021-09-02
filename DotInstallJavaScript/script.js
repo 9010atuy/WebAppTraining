83a23bdc0463e8f148ff888ef0e55ee5bd3aab21
yuta <9010atuy@gmail.copm>
2021-09-02 22:43:50 +0900

    add input, focus

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -1,38 +1,17 @@
 'use strict';
-const add = document.getElementById('add');
-const copy = document.getElementById('copy');
-const delRemove = document.getElementById('remove');
-const delNode = document.getElementById('delete-node');
 
 const ul = document.querySelector('ul');
-
+const input = document.getElementById('input');
+const addBtn = document.getElementById('add');
 let counter = 1;
-const addF = () => {
-  const li = document.createElement('li');
-  li.textContent = `list${counter}`;
-  li.id = `list${counter}`;
-  ul.appendChild(li);
+const addList = () => {
+  const list = document.createElement('li');
+  list.id = `list${counter}`;
+  list.textContent = input.value;
+  ul.appendChild(list);
+  input.value = '';
+  input.focus();
   counter++;
 };
 
-const copyF = () => {
-  const list3 = document.querySelectorAll('li')[2];
-  const copyElem = list3.cloneNode('true');
-  const list5 = document.querySelectorAll('li')[4];
-  ul.insertBefore(copyElem, list5);
-};
-
-const deleteRemoveF = () => {
-  const firstList = document.querySelectorAll('li')[0];
-  firstList.remove();
-};
-
-const delNodeF = () => {
-  const targeNode = document.querySelectorAll('li')[3];
-  ul.removeChild(targeNode);
-};
-
-add.addEventListener('click', addF);
-copy.addEventListener('click', copyF);
-delRemove.addEventListener('click', deleteRemoveF);
-delNode.addEventListener('click', delNodeF);
+addBtn.addEventListener('click', addList);
