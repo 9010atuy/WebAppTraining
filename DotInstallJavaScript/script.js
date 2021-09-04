69c7a36e66b0bc731230d82b81664443b7d97f41
yuta <9010atuy@gmail.copm>
2021-09-04 13:49:55 +0900

    add selectbox

--- DotInstallJavaScript/script.js
+++ DotInstallJavaScript/script.js
@@ -3,15 +3,11 @@
 const ul = document.querySelector('ul');
 const input = document.getElementById('input');
 const addBtn = document.getElementById('add');
-let counter = 1;
+
 const addList = () => {
-  const list = document.createElement('li');
-  list.id = `list${counter}`;
-  list.textContent = input.value;
-  ul.appendChild(list);
-  input.value = '';
-  input.focus();
-  counter++;
+  const li = document.createElement('li');
+  li.textContent = `${input.value}`;
+  ul.appendChild(li);
 };
 
 addBtn.addEventListener('click', addList);
