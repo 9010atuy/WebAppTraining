cbf6caec84716ec2b30dd89710f674b50633302c
yuta <9010atuy@gmail.copm>
2021-09-16 00:14:06 +0900

    add count up components

---
+++ vueTraining/sandbox-project/src/components/CountUp.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="count-up">
+  <h2>
+      クリックしたらカウントアップ
+  </h2>
+  <p>{{count}}</p>
+  <button v-on:click="countUp">カウント</button>
+</div>
+</template>
+<script>
+export default {
+    name:"CountUp",
+    data:function(){
+        return {
+            count:0
+        }
+    },
+    methods:{
+        countUp:function(){
+            this.count++;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
