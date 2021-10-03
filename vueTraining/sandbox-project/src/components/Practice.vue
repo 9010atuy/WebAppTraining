3ad7db87a15b224f7528a4226c88d26abe62d6a3
yuta <9010atuy@gmail.copm>
2021-10-03 13:07:21 +0900

    add v-if, v-else-if, v-else

--- vueTraining/sandbox-project/src/components/Practice.vue
+++ vueTraining/sandbox-project/src/components/Practice.vue
@@ -1,64 +1,26 @@
 <template>
     <div id="practice">
-        <a v-bind="google">Google</a>
-        <br>
-        <button v-on:click="countUp">click me!</button>
-        <p>{{methodCount}}</p>
-        <p>{{descriptCounterMethod(3)}}</p>
-        <p>{{computedCount}}</p>
-        <p>{{descriptCounterComputed(3)}}</p>
-        <p v-on:mousemove="getMousePosition">マウスを動かしてみて</p>
-        <p>x: {{eventObj.x}}</p>
-        <p>y: {{eventObj.y}}</p>
+        <div v-if="result">resulttrue!</div>
+        <div v-else-if="check">checktrue!</div>
+        <div v-else>false!</div>
     </div>
 </template>
     
 <script>
 export default {
-   name: "Practice",
-    data: function(){
-        return{
-            google:{href: "https://google.com", id:"google-link"},
-            eventObj:{x: 0, y: 0},
-            methodCount: 0,
-            computedCount: 0,
-        }
-    },
-    methods: {
-        countUp:function(){
-            this.methodCount++;
-        },
-        getMousePosition:function(event){
-            this.eventObj.x = event.x;
-            this.eventObj.y = event.y;
-        },
-        descriptCounterMethod:function(count){
-            if(this.methodCount > count){
-                this.msg = `counter: more than ${count}`;
-            } else {
-                this.msg = `counter: less than ${count}`;
-            }
-            return this.msg;
-        }
-    },
-    computed: {
-        descriptCounterComputed:function(){
-            let number = this;
-            let msg;
-            if(this.computedCount > number){
-                msg = `counter: more than ${number}`;
-            } else {
-                msg = `counter: less than ${number}`;
-            }
-            return msg;
+    name: "Practice",
+    data:function(){
+        return {
+            result: false,
+            check:true
         }
     }
-}
+   }
 </script>
     
 <style>
-     #google-link{
-         font-size:32px;
-         text-decoration: none;
-     }
+    div{
+        font-size: 32px;
+        font-weight: bold;
+    }
 </style>
