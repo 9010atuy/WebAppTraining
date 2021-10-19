96b1b2061f74acd5e9f1c50e97f6551670908611
yuta <9010atuy@gmail.copm>
2021-10-20 00:16:01 +0900

    add some vue function

--- vueTraining/sandbox-project/src/App.vue
+++ vueTraining/sandbox-project/src/App.vue
@@ -1,29 +1,21 @@
 <template>
   <div id="app">
-    <Practice/>
+    <Product/>
 
   </div>
 </template>
 
 <script>
-import Practice from './components/Practice.vue'
+import Product from './components/Product.vue'
 
 
 export default {
   name: 'App',
   components: {
-    Practice
+    Product
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ vueTraining/sandbox-project/src/components/ProductHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="product-header">
+        <h2>Apple 2021 New Arrival</h2>
+    </div>
+</template>
